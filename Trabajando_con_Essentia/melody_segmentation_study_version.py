b30442645b46bd148d689a210351dde718e0f983
Diego <diego.vid.eco@gmail.com>
2020-06-26 00:47:22 -0500

    Refactors melody_segmentation file

--- Trabajando_con_Essentia/melody_segmentation_study_version.py
+++ Trabajando_con_Essentia/melody_segmentation_study_version.py
@@ -1,100 +1,94 @@
+import sys
+import glob
+import os
+import numpy
+from essentia.standard import MonoLoader, PredominantPitchMelodia, MonoWriter
 from pylab import plot, show, figure, imshow
 import matplotlib.pyplot as plt
 import numpy as np
 import essentia
 import essentia.standard as es
-plt.rcParams['figure.figsize'] = (15, 6) # set plot sizes to something larger than default
-from essentia.standard import *
-import numpy
-import os
-import glob
-import sys
+# set plot sizes to something larger than default
+plt.rcParams['figure.figsize'] = (15, 6)
 
 sys.setrecursionlimit(20000)
 ###############################Melody detection#################################
 
-#Create folder for segments
-
-folder = 'Segments/'
-if not os.path.exists(folder):
-		os.makedirs(folder)
+# Create folder for segments
 
 sr = 44100
 hS = 512
-fS=2048
+fS = 2048
 
-def concat_audio (frame_count, segment_count, segment_list,audio_list, file_index):
-    print("frame, segment", frame_count,segment_count, file_index)
-    print("len", len(segment_list), segment_count)
-    if frame_count < len(segment_list[segment_count])-1:
-        startFrame = segment_list[segment_count][frame_count]
-        endFrame = segment_list[segment_count][frame_count + 1]
-        filename = folder + '{:04d}'.format(file_index) + '_' + "segment.wav"
-        print(filename)
-        MonoWriter(filename=filename,  format='wav', sampleRate=sr)(audio_list[segment_count][startFrame:endFrame])
-        concat_audio(frame_count+1, segment_count, segment_list, audio_list,file_index+1)
-    else:
-        if segment_count < len(segment_list)-1:
-            concat_audio(0, segment_count+1, segment_list, audio_list, file_index)
 
-#Extract the pitch curve
-#PitchMelodia takes the entire audio signal as input (no frame-wise processing is required)
+# Extract the pitch curve
+# PitchMelodia takes the entire audio signal as input (no frame-wise processing is required)
+
+
 def audio_segments_generator(audio):
-    pitch_extractor = PredominantPitchMelodia(frameSize=fS, 
-                                            hopSize=hS, 
-                                            harmonicWeight=0.70,     #harmonic weighting parameter (weight decay ratio between two consequent harmonics, =1 for no decay)
-                                            #magnitudeThreshold=40, #    spectral peak magnitude threshold (maximum allowed difference from the highest peak in dBs)
-                                            #guessUnvoiced=True,
-                                            minDuration=500, #    the minimum allowed contour duration [ms]
-                                            numberHarmonics=20,
-                                            timeContinuity=100,             #the maximum allowed gap duration for a pitch contour
-                                            voiceVibrato=True,
-                                            #voicingTolerance=0.5,    #allowed deviation below the average contour mean salience of all contours (fraction of the standard deviation)
-                                            #pitchContinuity=27.5625, #  27.5625  pitch continuity cue (maximum allowed pitch change during 1 ms time period) [cents]
-                                            #peakFrameThreshold=0.5 #0.45 funcionó para bach partita de violin per-frame salience threshold factor (fraction of the highest peak salience in a frame)
-
-
-                                            ) #These are samples!!! To write in audio we need to use frames
-
+    pitch_extractor = PredominantPitchMelodia(frameSize=fS,
+                                              hopSize=hS,
+                                              # harmonic weighting parameter (weight decay ratio between two consequent harmonics, =1 for no decay)
+                                              harmonicWeight=0.70,
+                                              # magnitudeThreshold=40, #    spectral peak magnitude threshold (maximum allowed difference from the highest peak in dBs)
+                                              # guessUnvoiced=True,
+                                              # the minimum allowed contour duration [ms]
+                                              minDuration=500,
+                                              numberHarmonics=20,
+                                              timeContinuity=100,  # the maximum allowed gap duration for a pitch contour
+                                              voiceVibrato=True,
+                                              # voicingTolerance=0.5,    #allowed deviation below the average contour mean salience of all contours (fraction of the standard deviation)
+                                              # pitchContinuity=27.5625, #  27.5625  pitch continuity cue (maximum allowed pitch change during 1 ms time period) [cents]
+                                              # peakFrameThreshold=0.5 #0.45 funcionó para bach partita de violin per-frame salience threshold factor (fraction of the highest peak salience in a frame)
+                                              )  # These are samples!!! To write in audio we need to use frames
     # Pitch is estimated on frames. Compute frame time positions
-    pitch_values, pitch_confidence = pitch_extractor(audio)
-    pitch_times = numpy.linspace(0.0, len(audio)/sr, len(pitch_values) ) # Return seconds
-    prev_index=0
-    elements = []
-    #Filter segments from data retrived
+    pitch_values, _ = pitch_extractor(audio)
+    prev_index = 0
+    sample_indexes = []
+    # Filter segments from data retrived
     for i in range(len(pitch_values)):
         num = pitch_values[i]
         if num > 0 and prev_index == 0:
-            elements.append(i)
+            sample_indexes.append(i)
         prev_index = num
-    # Convert the indexes into samples
-    sample_segments = [n*hS for n in elements]
+    # Convert the sample_indexes into samples
+    sample_list = [n*hS for n in sample_indexes]
     audioSize = audio.size
-    sample_segments.insert(0,0)
-    sample_segments.append(audioSize)
-    ##############################################################
-    #Plot the estimated pitch contour and confidence over time
+    sample_list.insert(0, 0)
+    sample_list.append(audioSize)
+    return sample_list
 
-    # f, axarr = plt.subplots(2, sharex=True)
-    # axarr[0].plot(pitch_times, pitch_values)
-    # axarr[0].set_title('estimated pitch [Hz]')
-    # axarr[1].plot(pitch_times, pitch_confidence)
-    # axarr[1].set_title('pitch confidence')
-    # plt.show()
-    # print('Amount of Segments:', sample_segments)
-
-    return sample_segments
-
-allSampleList = []
-allAudio = []
 
 # Load audio files from folder
+def concat_audio(out_dir, frame_count, segment_count, segment_list, audio_list, file_index):
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    files = []
+    print("frame, segment", frame_count, segment_count, file_index)
+    if frame_count < len(segment_list[segment_count])-1:
+        startFrame = segment_list[segment_count][frame_count]
+        endFrame = segment_list[segment_count][frame_count + 1]
+        filename = os.path.abspath(
+            os.path.join(out_dir, str(file_index)+".wav"))
+        files.append(
+            {"path": filename, "start": startFrame, "end": endFrame})
+        MonoWriter(filename=filename,  format='wav', sampleRate=sr)(
+            audio_list[segment_count][startFrame:endFrame])
+        concat_audio(out_dir, frame_count+1, segment_count,
+                     segment_list, audio_list, file_index+1)
+    else:
+        if segment_count < len(segment_list)-1:
+            concat_audio(out_dir, 0, segment_count+1, segment_list,
+                         audio_list, file_index)
+    return files
 
-for audio_files in glob.glob( 'audio/5/' + "*.wav" ):
-    audio = MonoLoader(filename=audio_files)()
+
+def process_file(out_dir, filename):
+    allSampleList = []
+    allAudio = []
+    audio = MonoLoader(filename=filename)()
     sampleSegments = audio_segments_generator(audio)
     allSampleList.append(sampleSegments)
     allAudio.append(audio)
-print("samples", allSampleList)
-
-concat_audio(0,0,allSampleList,allAudio,0)
+    files = concat_audio(out_dir, 0, 0, allSampleList, allAudio, 0)
+    return files
