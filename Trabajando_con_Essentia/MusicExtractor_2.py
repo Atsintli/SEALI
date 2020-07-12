e9ea2dc6c18d025779fbb1e85e2c90d17a317082
Aaron <atsintli@riseup.net>
2020-07-12 16:44:33 -0500

    ajustes trabajando con essentia

--- Trabajando_con_Essentia/MusicExtractor_2.py
+++ Trabajando_con_Essentia/MusicExtractor_2.py
@@ -9,27 +9,27 @@ import os
 file_name = 'features2.csv'
 file = open(file_name, 'w')
 
-# def get_className(path):
-#     with os.scandir(path) as entries:
-#         for entry in entries:
-#             print(entry.name)
-#         return(entry.name)
+def get_className(path):
+    with os.scandir(path) as entries:
+        for entry in entries:
+            print(entry.name)
+        return(entry.name)
 
-# def get_fileName_class_name(path):
-#     for dirpath, dirnames, files in os.walk(path):
-#         file_count = len(files)
-#         print(file_count)
-#         # print('Found directory:', dirpath)        
-#         # if len(dirnames) > 1:
-#         #     print('Number of classes:', len(dirnames))       
-#         # print('Number of files:', file_count)
-#         print(dirnames)
-#         for file_name in files:
-#              print(file_name)
-#         #     for classname in (dirnames):
-#         #         print('Class Name:', classname) 
-#         # print('\n')
-#         return(file_count, dirnames)
+def get_fileName_class_name(path):
+    for dirpath, dirnames, files in os.walk(path):
+        file_count = len(files)
+        print(file_count)
+        # print('Found directory:', dirpath)        
+        # if len(dirnames) > 1:
+        #     print('Number of classes:', len(dirnames))       
+        # print('Number of files:', file_count)
+        print(dirnames)
+        for file_name in files:
+             print(file_name)
+        #     for classname in (dirnames):
+        #         print('Class Name:', classname) 
+        # print('\n')
+        return(file_count, dirnames)
 
 list = []
 
@@ -41,25 +41,21 @@ for dirpath, dirnames, files in os.walk('audio'):
         file_count = len(files)
         list.append((dirnames, file_count))
 print(list)
-'''
 
-'''
 for dirpath, dirnames, files in os.walk('audio'):
     #files = [n for n in files]
     file_count = [len(files)]
     contents = dirnames + file_count
     list.append((contents))
     print (list)
-'''
 
-'''
 for dirpath, dirnames, files in os.walk('audio'):
     files = [n for n in files]
     file_count = len(files)
     #contents = dirnames + file_count
     list.append((dirnames, file_count))
     print(list)
-'''
+
 for dirpath, dirnames, files in os.walk('audio'):
     #files = [n for n in files]
     #file_count = [len(files)]
@@ -70,64 +66,69 @@ for dirpath, dirnames, files in os.walk('audio'):
     for file in files:
         print(files)
 
+get_fileName_class_name('audio')
+print(get_fileName_class_name('audio/'))
 
-#get_fileName_class_name('audio')
-#print(get_fileName_class_name('audio/'))
+def loop_data(path):
+    path, dirs, files = next(os.walk(path))
+    file_count = len(files)
+    for i in class_name():
+        print(file_count)
 
-# def loop_data(path):
-#     path, dirs, files = next(os.walk(path))
-#     file_count = len(files)
-#     for i in class_name():
-#         print(file_count)
+for dirpath, dirnames, files in os.walk('audio/', topdown=False):
+    print('Found directory:', dirpath)
+    for file_name in files:
+        print(file_name)
+'''
 
-# for dirpath, dirnames, files in os.walk('audio/', topdown=False):
-#     print('Found directory:', dirpath)
-#     for file_name in files:
-#         print(file_name)
+# Compute all features, aggregate only 'mean' and 'stdev' statistics for all low-level, rhythm and tonal frame features
+def feature_extractor(audio_files):
+    f, features_frames = es.MusicExtractor(
+        lowlevelStats=['mean', 'stdev'],
+        rhythmStats=['mean', 'stdev'],
+        tonalStats=['mean', 'stdev'])(audio_files)
 
+    print("Filename:", f['metadata.tags.file_name'])
+    #print(sorted(f.descriptorNames()))
 
-# # Compute all features, aggregate only 'mean' and 'stdev' statistics for all low-level, rhythm and tonal frame features
-# def feature_extractor(audio_files):
-#     f, features_frames = es.MusicExtractor(
-#         lowlevelStats=['mean', 'stdev'],
-#         rhythmStats=['mean', 'stdev'],
-#         tonalStats=['mean', 'stdev'])(audio_files)
+    loudness = f['lowlevel.average_loudness']
+    barkbands = f['lowlevel.barkbands.mean']
+    tuning = f['tonal.tuning_frequency']
+    flatness = f['lowlevel.barkbands_flatness_db.mean']
+    onset = f['rhythm.onset_rate']
+    rhythm = f['rhythm.bpm_histogram']
+    #beats_count = f['rhythm.beats_count'] # la escala esta por encima de 1 habŕia que normalizar los valores
+    mel = f['lowlevel.melbands_crest.mean']
+    mfcc = f['lowlevel.mfcc.mean']
+    entropy = f['lowlevel.spectral_entropy.mean']
 
-#     print("Filename:", f['metadata.tags.file_name'])
-#     #print(sorted(f.descriptorNames()))
+    #features = np.hstack([loudness,barkbands,tuning,flatness,onset,mel,mfcc,entropy, rhythm])
+    features = mfcc
+    n_descriptors = len(features)
+    print('Number of descriptors: ' + str(n_descriptors))
 
-#     loudness = f['lowlevel.average_loudness']
-#     barkbands = f['lowlevel.barkbands.mean']
-#     tuning = f['tonal.tuning_frequency']
-#     flatness = f['lowlevel.barkbands_flatness_db.mean']
-#     onset = f['rhythm.onset_rate']
-#     rhythm = f['rhythm.bpm_histogram']
-#     #beats_count = f['rhythm.beats_count'] # la escala esta por encima de 1 habŕia que normalizar los valores
-#     mel = f['lowlevel.melbands_crest.mean']
-#     mfcc = f['lowlevel.mfcc.mean']
-#     entropy = f['lowlevel.spectral_entropy.mean']
+    #class_name('audio')
+    i=0
+    n_files=7
+    class_name=0
+    list = []
+    list = map(str, features)
+    file = open(file_name, 'a')
+    while i < n_files: 
+        for c in range(n_files): #class_name('audio'):
+            print ('class_name:', c)
+        file.write(', '.join(list) + ', ' + str(class_name) + '\n') # el cero sería la clase
+        file.close()
 
-#     features = np.hstack([loudness,barkbands,tuning,flatness,onset,mel,mfcc,entropy, rhythm])
-#     n_descriptors = len(features)
-#     print('Number of descriptors: ' + str(n_descriptors))
+def get_dataset():
+    for audio_files in glob.glob('audioClases/Clase_5/' + "*.wav" ):
+        print(audio_files)
+        feature_extractor(audio_files)
 
-#     #class_name('audio')
+get_dataset()
 
-#     list = []
-#     list = map(str, features)
-#     file = open(file_name, 'a')
-#     while i < n_files: 
-#         for c in class_name('audio'):
-#             print ('class_name:', c)
-#         file.write(', '.join(list) + ', i' + '\n') # el cero sería la clase
-#         file.close()
+#feature_extractor('audio/ejemploCooper.wav')
 
-# #def get_dataset():
-# for audio_files in glob.glob('audio/1' + "/" + "*.wav" ):
+# for audio_files in glob.glob( 'audio' + "/" + "*.wav" ):
 #     feature_extractor(audio_files)
-
-# #feature_extractor('audio/dumitrescu.wav')
-
-# # for audio_files in glob.glob( 'audio' + "/" + "*.wav" ):
-# #     feature_extractor(audio_files)
 
--- Trabajando_con_Essentia/mean-shift-clustering_2.py
+++ Trabajando_con_Essentia/mean-shift-clustering_2.py
@@ -11,12 +11,26 @@ import matplotlib.pyplot as plt
 from itertools import cycle
 import shutil
 
+<<<<<<< Updated upstream
 
 def meanShift(features):  # features is [[float]]
     X, _ = make_blobs(n_samples=len(features), n_features=13,
                       centers=features, cluster_std=1.0)
     # Compute clustering with MeanShift
     # The following bandwidth can be automatically detected using
+=======
+def meanShift():
+	points = loadtxt('mfccs.csv')
+	X, _ = make_blobs(n_samples=len(points), n_features=13, centers=points, center_box=(0, 1), cluster_std=0.7)
+	# Compute clustering with MeanShift
+	# The following bandwidth can be automatically detected using
+
+	bandwidth = estimate_bandwidth(X, quantile=0.12, n_samples=2300)
+	ms = MeanShift(bandwidth=bandwidth, bin_seeding=False, max_iter=1000,cluster_all=True)
+	ms.fit(X)
+	labels = ms.labels_
+	cluster_centers = ms.cluster_centers_
+>>>>>>> Stashed changes
 
     bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=10)
     ms = MeanShift(bandwidth=bandwidth, bin_seeding=True,
@@ -25,11 +39,19 @@ def meanShift(features):  # features is [[float]]
     labels = ms.labels_
     cluster_centers = ms.cluster_centers_
 
+<<<<<<< Updated upstream
     labels_unique = np.unique(labels)
     n_clusters_ = len(labels_unique)
 
     print("number of estimated clusters : %d" % n_clusters_)
     return n_clusters_, labels, cluster_centers, X
+=======
+	# Predict the cluster for all the samples
+	P = ms.predict(X)
+
+	print("number of estimated clusters : %d" % n_clusters_)
+	return n_clusters_, labels, cluster_centers, X, P
+>>>>>>> Stashed changes
 
 ###Write files into n clusters archives###
 
@@ -107,6 +129,7 @@ def moveToFolders(n_clusters_, labels):
 
 
 def ploter(n_clusters_, labels, cluster_centers, X):
+<<<<<<< Updated upstream
     plt.figure(1)
     plt.clf()
     colors = cycle('bgrcmyk')
@@ -127,3 +150,32 @@ a, b, c, d = meanShift(features)
 # writeFiles(n_clusters_=a, labels=b)
 # ploter(n_clusters_=a, labels=b, cluster_centers=c, X=d)
 moveToFolders(a, b)
+=======
+	plt.figure(1)
+	plt.clf()
+	colors = cycle('bgrcmyk')
+	for k, col in zip(range(n_clusters_), colors):
+			my_members = labels == k
+			cluster_center = cluster_centers[k]
+			plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
+			plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+							markeredgecolor='k', markersize=14)
+	plt.title('Estimated number of clusters: %d' % n_clusters_)
+	plt.show()
+
+def ploter2(n_clusters_, X, P):
+		# Generate scatter plot for training data
+	colors = list(map(lambda x: '#3b4cc0' if x == 1 else '#b40426' if x == 2 else '#67c614', P))
+	plt.scatter(X[:,0], X[:,1], c=colors, marker="o", picker=True)
+	plt.title(f'Estimated number of clusters = {n_clusters_}')
+	plt.xlabel('Temperature yesterday')
+	plt.ylabel('Temperature today')
+	plt.show()
+
+a,b,c,d,f = meanShift()
+#writeFiles(n_clusters_=a, labels=b)
+ploter(n_clusters_=a,labels=b,cluster_centers=c,X=d)
+#ploter2(n_clusters_=a,X=d, P=f)
+
+moveToFolders(a,b)
+>>>>>>> Stashed changes
