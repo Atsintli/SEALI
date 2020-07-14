2faae6723a3e810243e5b9699d84fbed03eddd3d
Aaron <atsintli@riseup.net>
2020-07-14 01:18:18 -0500

    add some features

--- Trabajando_con_Essentia/realtime_feature_extraction.py
+++ Trabajando_con_Essentia/realtime_feature_extraction.py
@@ -7,13 +7,22 @@ from essentia.streaming import *
 from essentia import Pool, run, array, reset
 from scipy.special import softmax
 
+#OSC libs
+import argparse
+import math
+import requests # importing the requests library 
+from pythonosc import dispatcher
+from pythonosc import osc_server
+from pythonosc import udp_client
+import json
+
 sampleRate = 16000
 frameSize = 512 
 hopSize = 256
 numberBands = 13
 
 # analysis parameters
-patchSize = 64 #control of velocity of the extractor
+patchSize = 66 #control of velocity of the extractor 60 is approximately one second of audio
 displaySize = 10
 
 bufferSize = patchSize * hopSize
@@ -22,43 +31,47 @@ vectorInput = VectorInput(buffer)
 frameCutter = FrameCutter(frameSize=frameSize, hopSize=hopSize)
 w = Windowing(type = 'hann')
 spec = Spectrum()
-mfcc = MFCC()
+mfcc = MFCC(numberCoefficients=19)
+loudness = Loudness()
 
 pool = Pool()
 
-vectorInput.data   >> frameCutter.signal
-frameCutter.frame  >>  w.frame >> spec.frame 
-spec.spectrum >> mfcc.spectrum
-mfcc.bands >> None
-mfcc.mfcc >> (pool, 'lowlevel.mfcc.mean')
+vectorInput.data  >> frameCutter.signal
+frameCutter.frame >>  w.frame >> spec.frame 
+spec.spectrum     >> mfcc.spectrum
+mfcc.bands        >> None
+mfcc.mfcc         >> (pool, 'lowlevel.mfcc.mean')
+
+# falta agregar loudness!!!
+#print((frameCutter))
+#frameCutter.frame  >>  w.frame >> spec.frame 
+#vectorInput.data  >> loudness.signal >> (pool, 'lowlevel.loudness')
 
 def callback(data):
     # update audio buffer
     buffer[:] = array(unpack('f' * bufferSize, data))
-    print ("this is the buffer", buffer[:])
-    
-    #mfccBuffer = np.zeros([numberBands, patchSize * displaySize])
+    #print ("this is the buffer", buffer[:])
     mfccBuffer = np.zeros([numberBands])
-    print ("np zeros",mfccBuffer)
-
-    # generate predictions
     reset(vectorInput)
     run(vectorInput)
-    print('Pool contains %d frames of MFCCs' % len(pool['lowlevel.mfcc.mean']))
-    print (buffer)
-
-    # update mel and activation buffers
+    #print('Pool contains %d frames of MFCCs' % len(pool['lowlevel.mfcc.mean']))
     mfccBuffer = np.roll(mfccBuffer, -patchSize)
     mfccBuffer = pool['lowlevel.mfcc.mean'][-patchSize].T
-    print ("mfccs", mfccBuffer)
+    print ("MFCCs:", '\n', mfccBuffer)
+    mfccBufferList = []
+    mfccBufferList.append(mfccBuffer.tolist())
+    return mfccBufferList
 
-    return mfccBuffer
-
-#reset storage
-pool.clear()
+def tf_handler(args):
+  headers = {"content-type": "application/json"}
+  data = {"instances": [*args]}
+  r = requests.post(url = "http://localhost:8501/v1/models/improv_class:predict", data=json.dumps(data), headers=headers)
+  data = r.json()
+  print ('\n', data)
 
 # capture and process the speakers loopback
 # the 2 selects the external interface Zoom h5
 with sc.all_microphones(include_loopback=True)[2].recorder(samplerate=sampleRate) as mic:
-    while True:
-        callback(mic.record(numframes=bufferSize).mean(axis=1))
+  while True:
+    tf_handler(callback(mic.record(numframes=bufferSize).mean(axis=1)) )
+    #print ('\n', prediction)
